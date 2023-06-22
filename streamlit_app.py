83bdbc22465cd7b702307b1c8a733c8341e7f67b
aliraza-19 <118739564+aliraza-19@users.noreply.github.com>
2023-06-23 00:35:40 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,4 @@ st.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+st.text(fruityvice_response)
