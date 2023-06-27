aa69ee1dff5aab203fcc037b0c68a69d237652d6
aliraza-19 <118739564+aliraza-19@users.noreply.github.com>
2023-06-27 05:06:37 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('My Mom\'s New Healthy Diner')
-st.header("Breakfast Favorites")
+st.header('Breakfast Favorites')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
 st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
